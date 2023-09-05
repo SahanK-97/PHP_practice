cd9c406e3cb6b1ba372a409f382b894419c39364
Sahan Kanishka <76841019+SahanK-97@users.noreply.github.com>
2023-09-05 13:28:41 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,95 @@
+<?php
+
+echo "Hello World!";
+
+$name = "Sahan"; 
+echo '<br>';
+echo "Hello ".$name;
+
+echo '<br>';
+
+CONST NUMBER_OF_STUDENT = 50;
+
+echo NUMBER_OF_STUDENT;
+echo '<br>';
+$a = 5;
+$b = 10;
+$c = 20;
+
+$total = $a + $b;
+print($total);
+echo '<br>';
+print($a + $c);
+
+echo '<br>';
+print($a - $c);
+
+echo '<br>';
+print($a * $c);
+
+echo '<br>';
+print($a / $c);
+
+$string = "Some text content";
+$integer = 45;
+$float = 1.5;
+$boolean = true;
+$array = array(
+    "human",
+    "kamal",
+    "nimal"
+);
+
+print_r($array);
+ echo '<hr>';
+
+ echo strlen("Hi My name is Sahan");
+ echo "<br>";
+ echo str_word_count("Hi my name is Sahan");
+ echo "<br>";
+ echo strrev("How are you");
+ echo "<br>";
+ echo str_replace("Kanishka", "Sahan", "My name is Kanishka");
+ 
+ echo "<br>";
+
+ $x = 656;
+ var_dump(is_int($x));
+ $y = 65.25;
+ echo "<br>";
+ var_dump(is_int($y));
+ echo "<br>";
+
+ $z = (min(23,45,67,54,-45));
+ echo $z;
+ echo "<br>";
+ $d = (max(87,56,44,11,77));
+ echo $d;
+ echo "<br>";
+ echo (pi());
+ echo "<br>";
+ echo (abs(-888));
+ echo "<br>";
+ echo sqrt(55);
+ echo "<br>";
+ echo sqrt(49);
+ echo "<br>";
+ echo round(4.6);
+ echo "<br>";
+ echo rand();
+ echo "<br>";
+ echo (rand(1,50));
+ echo "<br>";
+ 
+
+
+
+
+ echo "<hr>";
+
+
+
+
+
+
+?>
